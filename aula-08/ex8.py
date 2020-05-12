5e0ad7af08e4e46d644ed9b0065d7eab00dc4c05
thiagobeppe <thiagoabeppe@gmail.com>
2020-05-12 11:55:29 -0300

    finish the exercise

--- aula-08/ex8.py
+++ aula-08/ex8.py
@@ -1,43 +1,38 @@
 import cv2
 import numpy as np
 
+#Reads the file
 imgOriginal = cv2.imread("ex8.jpg", 0)
-print(imgOriginal)
+#Trying to reforce the board - Choose the median and laplacian filter to detect the board
+imgTratada = cv2.medianBlur(imgOriginal,7)
+imgFiltrada = cv2.Laplacian(imgTratada, cv2.CV_8U)
+imgRealcada = cv2.subtract(imgOriginal, imgFiltrada)
+
+#Applying the adapative binarization because the low luminosity
 metodo = cv2.THRESH_BINARY_INV
-ret, imgBinarizada = cv2.threshold(imgOriginal, 238, 255, metodo)
+imgBinarizada = cv2.adaptiveThreshold(imgRealcada, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,metodo, 11, 5)
+
+
+# Confs for morphologic methods
+typesMorph = [cv2.MORPH_ELLIPSE, cv2.MORPH_CROSS, cv2.MORPH_RECT]
+elementoEstruturante = cv2.getStructuringElement(typesMorph[0], (4,4))
 
 #Erosão
-elementoEstruturante = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
 imgProcessadaErode = cv2.erode(imgBinarizada, elementoEstruturante, iterations=1)
-# cv2.imshow("Imagem Binarizada", imgBinarizada)
-# cv2.imshow('Erosao', imgProcessadaErode)
-# cv2.waitKey(0)
+
 
 # #Dilatação
 imgProcessadaDilate = cv2.dilate(imgBinarizada, elementoEstruturante, iterations=1)
-# cv2.imshow("Imagem Binarizada", imgBinarizada)
-# cv2.imshow('Dilatacao', imgProcessadaDilate)
-# cv2.waitKey(0)
 
-# #Abertura
+
+#Abertura
 imgProcessadaOpen = cv2.morphologyEx(imgBinarizada, cv2.MORPH_OPEN, elementoEstruturante)
-# cv2.imshow("Imagem Binarizada", imgBinarizada)
-# cv2.imshow('Abertura', imgProcessadaOpen)
-# cv2.waitKey(0)
 
-# #Fechamento
+#Fechamento
 imgProcessadaClose = cv2.morphologyEx(imgBinarizada, cv2.MORPH_CLOSE, elementoEstruturante)
-# cv2.imshow("Imagem Binarizada", imgBinarizada)
-# cv2.imshow('Fechamento', imgProcessadaClose)
-# cv2.waitKey(0)
 
-#Tentativa
-elementoEstruturante = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
-imgProcessadaErode = cv2.erode(imgBinarizada, elementoEstruturante, iterations=1)
-imgProcessadaCloseAndErode = cv2.morphologyEx(imgProcessadaErode, cv2.MORPH_CLOSE, elementoEstruturante)
-imgProcessadaCloseAndErodeAndOpen = cv2.morphologyEx(imgProcessadaCloseAndErode, cv2.MORPH_OPEN, elementoEstruturante)
-cv2.imshow("Imagem Erodida", imgBinarizada)
-cv2.imshow('Processada Erosao + Fechamento', imgProcessadaCloseAndErode)
-cv2.imshow('Processada Erosao + Fechamento + Abertura', imgProcessadaCloseAndErodeAndOpen)
+
+# Detect the values on image
+imgFinal = cv2.dilate(cv2.morphologyEx(imgBinarizada, cv2.MORPH_CLOSE, elementoEstruturante), elementoEstruturante, iterations=1)
+cv2.imshow("Final Close + Dilate", imgFinal)
 cv2.waitKey(0)
-# cv2.destroyAllWindows()
